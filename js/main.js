d566ae2791c5cef7148c2081c014a76238b1fe0f
AriesChauh413 <ckzhian413@gmail.com>
2019-10-25 10:32:48 +0800

    Second edited new branch

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 
 <h2>My First JavaScript</h2>
 
-<button type="button"
+<button type="button123"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+Click me to display Date and Time .</button>
 
 <p id="demo"></p>
 
